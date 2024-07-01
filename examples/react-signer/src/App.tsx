764af95901c515876ee3f70502d9f827b8f48223
Vitalii Pustovyi <vitalii@identitylabs.ooo>
2024-07-01 15:47:36 +0300

    feat([sc-13513]): border added

--- examples/react-signer/src/App.tsx
+++ examples/react-signer/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="flex items-center justify-center h-[100vh]">
-      <div className="flex flex-col justify-center w-[450px] h-[640px] bg-white dark:bg-black">
+      <div className="flex flex-col justify-center w-[450px] h-[640px] bg-white dark:bg-black sm:border border-neutral-300 rounded-md">
         {State.LOADING === state || State.READY == state || !component ? (
           <div className="flex items-center justify-center">
             <Loader />
