c9c75b7cc749409d0c06549ed9438f8bfe7b64c9
Vitalii Pustovyi <vitalii@identitylabs.ooo>
2024-07-01 15:22:45 +0300

    feat([sc-13513]): signer popup responsive + window npm command

--- examples/react-signer/src/App.tsx
+++ examples/react-signer/src/App.tsx
@@ -5,14 +5,16 @@ function App() {
   const { component, state } = useSigner()
 
   return (
-    <div className="flex flex-col justify-center w-[450px] h-[640px] bg-white dark:bg-black">
-      {State.LOADING === state || State.READY == state || !component ? (
-        <div className="flex items-center justify-center">
-          <Loader />
-        </div>
-      ) : (
-        <div className="pt-[58px] pb-[30px] px-[20px] h-full flex flex-col">{component}</div>
-      )}
+    <div className="flex items-center justify-center h-[100vh]">
+      <div className="flex flex-col justify-center w-[450px] h-[640px] bg-white dark:bg-black">
+        {State.LOADING === state || State.READY == state || !component ? (
+          <div className="flex items-center justify-center">
+            <Loader />
+          </div>
+        ) : (
+          <div className="pt-[58px] pb-[30px] px-[20px] h-full flex flex-col">{component}</div>
+        )}
+      </div>
     </div>
   )
 }
