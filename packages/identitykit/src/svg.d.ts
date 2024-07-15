b4f94653417c4ce1d112bc11334484f1d95a8304
Vitalii Pustovyi <vitalii@identitylabs.ooo>
2024-07-15 16:44:49 +0300

    feat([sc-13515]): d.types for svg assets

--- packages/identitykit/src/svg.d.ts
+++ packages/identitykit/src/svg.d.ts
@@ -1,7 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module "*.svg" {
-  import * as React from "react"
-
-  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
-  const content: string
+  const content: any
   export default content
 }
