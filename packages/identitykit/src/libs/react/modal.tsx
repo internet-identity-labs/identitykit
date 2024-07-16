934473fc15bd55818c2cb9c379012f6376e4a830
Vitalii Pustovyi <vitalii@identitylabs.ooo>
2024-07-16 14:23:15 +0300

    feat([sc-13515]): customSigner review fixes

--- packages/identitykit/src/libs/react/modal.tsx
+++ packages/identitykit/src/libs/react/modal.tsx
@@ -6,25 +6,14 @@ import { IdentityKitTheme } from "./constants"
 import { Button } from "./ui/button"
 import InfoIcon from "./assets/info.svg"
 import BackIcon from "./assets/back.svg"
-import { useForm } from "react-hook-form"
-import { isValidURL } from "./utils"
+
 import { Tooltip } from "./ui/tooltip"
 import { TooltipProvider } from "@radix-ui/react-tooltip"
+import { CustomSignerInput } from "./ui/input"
 
 export const IdentityKitModal = (props: { theme: IdentityKitTheme }) => {
-  const { isModalOpen, selectedSigner, signers, selectSigner, setCustomSigner } =
-    useContext(IdentityKitContext)
+  const { isModalOpen, selectedSigner, signers, selectSigner } = useContext(IdentityKitContext)
   const [isViewAll, setIsViewAll] = useState(false)
-  const {
-    register,
-    formState: { errors },
-    watch,
-  } = useForm({
-    mode: "all",
-    defaultValues: {
-      url: "",
-    },
-  })
 
   useEffect(() => {
     setTimeout(() => {
@@ -36,13 +25,6 @@ export const IdentityKitModal = (props: { theme: IdentityKitTheme }) => {
       document.body.style.overflow = isModalOpen ? "hidden" : ""
     }, 0)
   }, [isModalOpen])
-
-  const customSignerUrl = watch("url")
-
-  const submitHandler = () => {
-    if (!customSignerUrl) return
-    setCustomSigner(customSignerUrl)
-  }
 
   // theme inherits from system by default
   const theme =
@@ -131,32 +113,7 @@ export const IdentityKitModal = (props: { theme: IdentityKitTheme }) => {
                     View all
                   </Button>
                 ) : (
-                  <div>
-                    <div className="text-l text-black font-bold my-[20px]">Custom Connect</div>
-                    <div className="flex items-center gap-[12px] relative">
-                      <input
-                        className="rounded-xl border border-gray-400 px-[10px] h-[48px] flex-1 flex-shrink text-sm text-gray-400 focus:border-gray-400"
-                        placeholder="https://wallet.url"
-                        type="text"
-                        {...register("url", {
-                          required: true,
-                          validate: (value) => isValidURL(value),
-                        })}
-                      />
-                      <Button
-                        disabled={Boolean(errors.url) || !customSignerUrl}
-                        className="w-[110px]"
-                        onClick={submitHandler}
-                      >
-                        Connect
-                      </Button>
-                      {errors.url && (
-                        <p className="absolute text-xs text-red-500 top-[100%]">
-                          {errors.url.message}
-                        </p>
-                      )}
-                    </div>
-                  </div>
+                  <CustomSignerInput />
                 )}
               </div>
               <div></div>
