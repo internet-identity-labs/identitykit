ff7495e5b79bf7c85ad27c30ca5fc57eb6b3fc3a
Dmitrii <105656449+dmitriiIdentityLabs@users.noreply.github.com>
2024-08-02 15:45:56 +0400

    Extend nfid identitykit config file for the [sc-13522] (#37)

---
+++ packages/identitykit/src/lib/service/transport-builder/extension-transport.builder.ts
@@ -0,0 +1,5 @@
+import { Transport } from "@slide-computer/signer"
+
+export function getExtensionTransportBuilder(): Transport {
+  throw new Error("getExtensionTransport function not implemented.")
+}
