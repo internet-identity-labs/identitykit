994345b932ce5d7dfec30c446af78297f4c1c8b5
olsemeno <33149496+olsemeno@users.noreply.github.com>
2024-08-22 12:15:23 +0200

    Revert "Fix getDelegation time for NFID Wallet on [sc-13925] (#74)" (#83)
    This reverts commit b270b0c4941f90ed52e5a0e8476c34b4411765d2.

--- examples/react-signer/src/idl/consent_idl.ts
+++ examples/react-signer/src/idl/consent_idl.ts
@@ -45,8 +45,6 @@ export const idlFactory = ({ IDL }: any) => {
     Err: icrc21_error,
   })
   const icrc28_trusted_origins_response = IDL.Record({
-    certificate: IDL.Vec(IDL.Nat8),
-    witness: IDL.Vec(IDL.Nat8),
     trusted_origins: IDL.Vec(IDL.Text),
   })
   return IDL.Service({
@@ -62,7 +60,7 @@ export const idlFactory = ({ IDL }: any) => {
       [icrc21_consent_message_response],
       []
     ),
-    icrc28_trusted_origins: IDL.Func([], [icrc28_trusted_origins_response], ["query"]),
+    icrc28_trusted_origins: IDL.Func([], [icrc28_trusted_origins_response], []),
   })
 }
 export const init = ({}) => {
