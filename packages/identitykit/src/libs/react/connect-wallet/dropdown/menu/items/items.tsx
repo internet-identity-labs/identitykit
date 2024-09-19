4901e3102b1872db217e6f16458afca683c56c6e
Oleksandr Diukarev <168409204+olekid@users.noreply.github.com>
2024-09-19 18:04:33 +0400

    fix: dropdown menu z-index (#157)

--- packages/identitykit/src/libs/react/connect-wallet/dropdown/menu/items/items.tsx
+++ packages/identitykit/src/libs/react/connect-wallet/dropdown/menu/items/items.tsx
@@ -10,7 +10,7 @@ export function Items({ className, innerClassName, children, ...props }: ItemsPr
       anchor="bottom end"
       transition
       className={clsx(
-        "ik-min-w-[320px] ik-rounded-3xl ik-bg-white dark:ik-bg-zinc-900 ik-shadow-lg ik-transition focus:ik-outline-none data-[closed]:ik-scale-95 data-[closed]:ik-transform data-[closed]:ik-opacity-0 data-[enter]:ik-duration-100 data-[leave]:ik-duration-75 data-[enter]:ik-ease-out data-[leave]:ik-ease-in ik-p-2.5 [--anchor-gap:8px]",
+        "ik-min-w-[320px] ik-rounded-3xl ik-bg-white dark:ik-bg-zinc-900 ik-shadow-lg ik-transition focus:ik-outline-none data-[closed]:ik-scale-95 data-[closed]:ik-transform data-[closed]:ik-opacity-0 data-[enter]:ik-duration-100 data-[leave]:ik-duration-75 data-[enter]:ik-ease-out data-[leave]:ik-ease-in ik-p-2.5 [--anchor-gap:8px] ik-z-[999]",
         className
       )}
       {...props}
