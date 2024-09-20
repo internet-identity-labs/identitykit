a727e25ac7dd53a214bd3f39f787591733da63ab
Oleksandr Diukarev <oleksandr@identitylabs.ooo>
2024-09-20 14:40:41 +0400

    feat([sc-15170]): theme moved to useEffect

--- packages/identitykit/src/libs/react/connect-wallet/dropdown/menu/items/items.tsx
+++ packages/identitykit/src/libs/react/connect-wallet/dropdown/menu/items/items.tsx
@@ -1,14 +1,19 @@
 import { MenuItems, MenuItemsProps } from "@headlessui/react"
 import clsx from "clsx"
+import { IdentityKitContext } from "../../../../context"
+import { useContext } from "react"
 
 export type ItemsProps = MenuItemsProps & { innerClassName?: string }
 
 export function Items({ className, innerClassName, children, ...props }: ItemsProps) {
   const menuItems = !Array.isArray(children) ? [children] : children
+  const { theme } = useContext(IdentityKitContext)
+
   return (
     <MenuItems
       anchor="bottom end"
       transition
+      data-identity-kit-theme={theme}
       className={clsx(
         "ik-min-w-[320px] ik-rounded-3xl ik-bg-white dark:ik-bg-zinc-900 ik-shadow-lg ik-transition focus:ik-outline-none data-[closed]:ik-scale-95 data-[closed]:ik-transform data-[closed]:ik-opacity-0 data-[enter]:ik-duration-100 data-[leave]:ik-duration-75 data-[enter]:ik-ease-out data-[leave]:ik-ease-in ik-p-2.5 [--anchor-gap:8px] ik-z-[999]",
         className
