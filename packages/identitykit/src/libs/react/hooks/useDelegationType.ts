f4e908cea68de19d67a947ca22488154e3f0c4fa
Oleksandr Diukarev <oleksandr@identitylabs.ooo>
2024-10-14 15:12:52 +0400

    feat([sc-15412]): general agent

--- packages/identitykit/src/libs/react/hooks/useDelegationType.ts
+++ packages/identitykit/src/libs/react/hooks/useDelegationType.ts
@@ -1,10 +1,10 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext } from "react"
 import { IdentityKitContext } from "../context"
-import { DelegationSignerClient, DelegationType } from "../../../lib/signer-client"
+import { DelegationSignerClient } from "../../../lib/signer-client"
 import { IdentityKitAuthType } from "../../../lib/identity-kit"
+import { useAsyncMemo } from "use-async-memo"
 
 export function useDelegationType() {
-  const [delegationType, setDelegetaionType] = useState<DelegationType | undefined>()
   const ctx = useContext(IdentityKitContext)
 
   if (!ctx) {
@@ -13,18 +13,10 @@ export function useDelegationType() {
 
   const { signerClient, authType, user } = ctx
 
-  useEffect(() => {
-    if (user) {
-      if (
-        authType === IdentityKitAuthType.DELEGATION &&
-        signerClient instanceof DelegationSignerClient
-      ) {
-        ;(signerClient as DelegationSignerClient)?.getDelegationType().then(setDelegetaionType)
-      }
-    } else {
-      setDelegetaionType(undefined)
-    }
-  }, [user, signerClient])
+  const delegationType = useAsyncMemo(() => {
+    if (!user || !signerClient || authType !== IdentityKitAuthType.DELEGATION) return undefined
+    return (signerClient as DelegationSignerClient)?.getDelegationType()
+  }, [user, authType, signerClient])
 
   return {
     delegationType,
