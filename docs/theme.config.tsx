0fafd6eba07945ebd6d52db4f462b38fe24cb84d
Oleksandr Diukarev <168409204+olekid@users.noreply.github.com>
2024-12-09 16:17:56 +0400

    fix: isInitializing true on first render after connected user reloads page (#256)

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -48,10 +48,10 @@ const theme = {
     light: 38,
   },
   banner: {
-    key: "1.0.2-release",
+    key: "1.0.3-release",
     content: (
       <a href="https://www.npmjs.com/package/@nfid/identitykit" target="_blank">
-        🎉 @nfid/identitykit@1.0.2 is released.
+        🎉 @nfid/identitykit@1.0.3 is released.
       </a>
     ),
   },
