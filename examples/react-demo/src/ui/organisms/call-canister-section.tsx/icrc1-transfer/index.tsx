dea1870e3c03cd5eeabcd7f76659549dd9803fd5
Oleksandr Diukarev <168409204+olekid@users.noreply.github.com>
2025-01-21 16:55:26 +0700

    ikit demo Canister ID field isn t parsed [sc-16547] (#284)

--- examples/react-demo/src/ui/organisms/call-canister-section.tsx/icrc1-transfer/index.tsx
+++ examples/react-demo/src/ui/organisms/call-canister-section.tsx/icrc1-transfer/index.tsx
@@ -86,7 +86,7 @@ export function Icrc1Transfer({ className }: { className?: string }) {
       request={{
         method: "icrc49_call_canister",
         params: {
-          canisterId: PEPE_LEDGER_CANISTER_ID,
+          canisterId: canister_id,
           sender: user?.principal.toString() || "",
           method: CallCanisterMethod.icrc1_transfer,
           arg: isFormValid ? toBase64(IDL.encode(transferIDL.argTypes, [actorArgs])) : "",
