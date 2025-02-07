2aa56bfcc3f3e49a1035db15452a81ff4147a370
Oleksandr Diukarev <oleksandr@identitylabs.ooo>
2025-02-07 20:20:32 +0700

    [17141] meta + button

--- landing/src/components/button.tsx
+++ landing/src/components/button.tsx
@@ -8,9 +8,18 @@ export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   loading?: boolean
   icon?: string
   text?: boolean
-  type?: "primary" | "secondary"
+  type?: "primary" | "secondary" | "outline"
   large?: boolean
   as?: React.ElementType
+}
+
+const buttonStyle = {
+  primary:
+    "component border-transparent bg-primary text-white hover:bg-teal-600 active:bg-teal-700 active:border-primary",
+  secondary:
+    "component border-white bg-[#FFEBFD1A] hover:bg-[#FFEBFD40] active:bg-[#FFEBFD1A] active:opacity-50 text-white",
+  outline:
+    "component border-black text-black bg-transparent hover:bg-gray-700 hover:border-gray-700 hover:text-white active:text-white active:bg-black active:border-black dark:border-white dark:hover:border-white dark:hover:bg-white dark:hover:text-black dark:active:border-gray-300 dark:active:bg-gray-300 dark:active:text-black dark:text-white",
 }
 
 const getButtonClassName = ({
@@ -19,15 +28,13 @@ const getButtonClassName = ({
   block,
 }: {
   className?: string
-  type?: "primary" | "secondary"
+  type?: "primary" | "secondary" | "outline"
   block?: boolean
   large?: boolean
 }) =>
   clsx(
     "transition-colors duration-200 hover:shadow-md font-bold px-[10px] min-w-[140px] h-[48px] flex items-center justify-center px-[15px] rounded-xl border",
-    type === "primary"
-      ? "component border-transparent bg-primary text-white hover:bg-teal-600 active:bg-teal-700 active:border-primary"
-      : "component border-white bg-[#FFEBFD1A] hover:bg-[#FFEBFD40] active:bg-[#FFEBFD1A] active:opacity-50 text-white",
+    buttonStyle[type],
     {
       "disabled:shadow-none disabled:bg-gray-300 disabled:border-gray-300 dark:disabled:bg-zinc-700 dark:disabled:border-zinc-700":
         type === "primary",
