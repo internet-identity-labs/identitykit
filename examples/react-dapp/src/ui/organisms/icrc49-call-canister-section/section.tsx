1b0f7e8499f04b6e05553c9d16e42f13f3000f6e
Dmytro <dmytroidentitylabs@gmail.com>
2026-06-04 10:59:53 +0400

    Upgrade all dfinity libraries to the latest [sc-18776]

--- examples/react-dapp/src/ui/organisms/icrc49-call-canister-section/section.tsx
+++ examples/react-dapp/src/ui/organisms/icrc49-call-canister-section/section.tsx
@@ -1,15 +1,23 @@
 import { Section as BaseSection } from "../section"
 import { useSigner } from "../../../../../../packages/identitykit/src/libs/react/hooks"
-import { CALL_CANISTER_METHODS, CallCanisterMethodType } from "./constants"
+import { CALL_CANISTER_METHODS } from "./constants"
+import type { Signer } from "@icp-sdk/signer"
 
-export function Section({
+type PartialSignerRequest = Omit<
+  Parameters<Signer["sendRequest"]>[0],
+  "jsonrpc" | "id" | "params"
+> & {
+  params?: Record<string, string>
+}
+
+export function Section<TRequest extends PartialSignerRequest>({
   getCodeSnippet,
   className,
   request,
 }: {
   getCodeSnippet: (params: { canisterId: string; method: string }) => string
   className?: string
-  request: any
+  request: TRequest
 }) {
   const signer = useSigner()
   return (
@@ -18,7 +26,7 @@ export function Section({
       request={request}
       id={"icrc49_call_canister-" + request.params?.method}
       getCodeSnippet={({ params }) => {
-        if (!CALL_CANISTER_METHODS.includes(params!.method as CallCanisterMethodType))
+        if (!CALL_CANISTER_METHODS.some((method) => method === params!.method))
           throw new Error("Method not supported")
         return getCodeSnippet({ canisterId: params!.canisterId, method: params!.method })
       }}
