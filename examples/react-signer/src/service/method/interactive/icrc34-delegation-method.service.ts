5a600e1f52dd98ef4f9740484241e2a49b759583
Dmytro <dmytroidentitylabs@gmail.com>
2026-09-14 22:50:36 +0400

    Update icp sdk Libraries for NFID IdentityKit [sc-19780]

--- examples/react-signer/src/service/method/interactive/icrc34-delegation-method.service.ts
+++ examples/react-signer/src/service/method/interactive/icrc34-delegation-method.service.ts
@@ -95,7 +95,7 @@ class Icrc34DelegationMethodService extends InteractiveMethodService {
         return {
           delegation: Object.assign(
             {
-              expiration: delegation.expiration,
+              expiration: delegation.expiration.toString(),
               pubkey: this.toBase64(delegation.pubkey),
             },
             targets && {
@@ -131,22 +131,16 @@ class Icrc34DelegationMethodService extends InteractiveMethodService {
       ? Number(icrc34Dto.maxTimeToLive) / NANOS_IN_MILLIS
       : MAX_TIME_TO_LIVE_MILLIS
 
-    if (accountKeyIdentity.type === AccountType.GLOBAL) {
-      const targets = icrc34Dto.targets.map((x) => Principal.fromText(x))
-
-      return await DelegationChain.create(
-        accountKeyIdentity.keyIdentity,
-        sessionPublicKey,
-        new Date(Date.now() + maxTimeToLive),
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        { targets: targets as any }
-      )
-    }
+    const targets = icrc34Dto.targets?.length
+      ? icrc34Dto.targets.map((x) => Principal.fromText(x))
+      : undefined
 
     return await DelegationChain.create(
       accountKeyIdentity.keyIdentity,
       sessionPublicKey,
-      new Date(Date.now() + maxTimeToLive)
+      new Date(Date.now() + maxTimeToLive),
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      targets && { targets: targets as any }
     )
   }
 
